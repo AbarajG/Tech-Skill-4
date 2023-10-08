9becec2ab48185c54a6ed1f8889bb75ac17524ea
AbarajG <andygomezmojica@gmail.com>
2023-10-08 14:47:47 -0600

    first commit

---
+++ src/test/java/com/andygomez/ShopAll/ShopAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andygomez.ShopAll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
